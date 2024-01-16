9c8800b5b1635b5c3a4ce8e4cb26933fb3e071b0
jindun619 <jindun619@naver.com>
2024-01-16 12:12:43 +0000

    font added: notoSansKR(google font)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
+import { Noto_Sans_KR } from "next/font/google";
+
 import { Layout } from "@/components/Layout";
+
+const notoSansKr = Noto_Sans_KR({ subsets: ["latin"] });
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <main className={notoSansKr.className}>
+        <Component {...pageProps} />
+      </main>
     </Layout>
   );
 };
