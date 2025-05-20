662579072feb86e859e28c4a6e408d8254954810
jindun619 <jindun619@naver.com>
2025-05-20 12:26:01 +0800

    refactor: remove admin functionality

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,6 @@
 import { remark } from "remark";
 import html from "remark-html";
 
-export const verifyToken = (token: string) => {
-  // 토큰 검증 로직 구현
-  return token === process.env.ADMIN_TOKEN;
-};
-
 export async function markdownToHtml(markdown: string) {
   const result = await remark().use(html).process(markdown);
   return result.toString();
